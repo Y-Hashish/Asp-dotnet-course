2ed9fe00609d7fa64af48089910fcaf3b0e76f0c
Y-Hashish <emady2706@gmail.com>
2025-11-25 01:23:24 +0200

    partial view

---
+++ Partial_Views/Partial Views/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Partial_Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
